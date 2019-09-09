a4fb0197382caf4ec0e649fb2e651ec2ec5fe9ba
ParanoidBat <41900396+ParanoidBat@users.noreply.github.com>
2019-09-09 16:59:08 +0500

    resolvinf sourcetree errors

--- VowelRecognizer.py
+++ VowelRecognizer.py
@@ -4,12 +4,15 @@ Created on Mon Jul 15 15:50:07 2019
 
 @author: Batman
 """
+<<<<<<< Updated upstream
 from python_speech_features import base
+=======
+>>>>>>> Stashed changes
 from scipy.io.wavfile import read
 import numpy as npy
 import matplotlib.pyplot as plt
 #import sounddevice as sd
-from scipy import fftpack
+#from scipy import fftpack
 from scipy import signal
 from math import floor
 
@@ -47,6 +50,7 @@ plt.title("Spectrogram - audio")
 #plt.ylim(0,10)
 plt.show()
 
+<<<<<<< Updated upstream
 #mel-frequency cepstral co-efficients (MFCC)
 freqs, px = signal.welch(audio, fs= rate_bit, window= "hamming", nperseg=400, noverlap=(400-160), nfft= 512, detrend= False, scaling= "spectrum")
 #plt.semilogy(freqs, px) #to plot
@@ -133,3 +137,52 @@ plt.show()
 #plt.ylabel("values")
 ##plt.xlim(24882, )
 #plt.show()
+=======
+#plot high density energy frames, take 5 element window
+average =[]
+for i in range(time.size):
+    if i+4 >= time.size: break
+    #take mean of matrix along x-axis
+    vector = npy.mean(sx[:170,i:i+4],1) #frequencies after index 170 are useless. As 8k Hz is the last formant of use
+    i+=5
+    #get a single value out of the vector, which represents a single value energy magnitude of the frame
+    vector_avg = npy.mean(vector)
+    average.append(vector_avg)
+    
+
+plt.plot(average)
+plt.title("Energy contour")
+plt.xlabel("index")
+plt.ylabel("energy")
+#plt.xlim(0,20 )
+plt.ylim(0,30000)
+plt.show()
+
+#recognize silence and non-silence regions
+#silent <1000, segment == 25000
+segment_vals = []
+i =0
+
+while i < len(average):
+    if average[i] >=25000.0:
+        print ("segmented at ", average[i])
+        if max(segment_vals) > 1000.0:
+            print("non-silence")
+            del segment_vals[:]
+        else:
+            print("silence")
+            del segment_vals[:]
+            
+        for j in range(i, len(average)):
+            if average[j] < 25000.0:
+                print ("segmented at ", average[j])
+                print ("non-silence")
+                del segment_vals[:]
+                i = j
+                break
+            
+            segment_vals.append(average[j])
+            
+    else: segment_vals.append(average[i])
+    i+=1
+>>>>>>> Stashed changes
