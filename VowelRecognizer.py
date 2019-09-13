2c6a3eaada50bb866584f6b52c0f68114518e802
ParanoidBat <41900396+ParanoidBat@users.noreply.github.com>
2019-09-13 20:10:29 +0500

    mark the segment initial values on energy contour

--- VowelRecognizer.py
+++ VowelRecognizer.py
@@ -9,16 +9,13 @@ import numpy as npy
 import matplotlib.pyplot as plt
 #import sounddevice as sd
 from scipy import signal
-from collections import deque
 
 def plotTimAmp(audio, rate):
-    plt.plot(npy.arange(audio.shape[0])/rate_bit,audio) # time-amplitude plot
-    plt.xlabel('Time (s)')
-    plt.ylabel('Amplitude')
-    plt.title('Plot - bit')
-    #plt.xlim(0.4,)
-    #plt.ylim(900,)
-    plt.show()
+     plt.plot(npy.arange(audio.shape[0])/rate_bit,audio) # time-amplitude plot
+     plt.title("Audio wave")
+     plt.xlabel("Time - s")
+     plt.ylabel("Amplitude")
+     plt.show()
     
 def plotSpec(audio, rate):
     freq, time, sx = signal.spectrogram(audio, fs=rate_about, window="hamming", nperseg=1024, noverlap=924, detrend=False, scaling="spectrum")
@@ -31,7 +28,7 @@ def plotSpec(audio, rate):
     
     return freq, time, sx
 
-def plotEnergyContour(time, spectogram):
+def energyFrames(time, spectogram):
     average =[]
     for i in range(time.size):
         if i+4 >= time.size: break
@@ -41,14 +38,6 @@ def plotEnergyContour(time, spectogram):
         #get a single value out of the vector, which represents a single value energy magnitude of the frame
         vector_avg = npy.mean(vector)
         average.append(vector_avg)
-        
-    plt.plot(average)
-    plt.title("Energy contour")
-    plt.xlabel("index")
-    plt.ylabel("energy")
-#    plt.xlim(200,250)
-#    plt.ylim(0,30000) 
-    plt.show()
     
     return average
 
@@ -56,7 +45,8 @@ def segment():
     #silent <1000, segment == 25000
     segments = []
     segment_vals = []
-    con_list = []
+    centre_vals = [] #centered b/w ith and kth index, when extending segment
+    seg_start = []
     i = k = 0
     
     while i < len(average):
@@ -69,13 +59,14 @@ def segment():
                     break
                 else:
                     k-=1
-                    
-            #while loop breaks where the transition is found, create a new list with both segments' values
-            u_seg_vals = average[k:i] + segment_vals[:]
             
             #store the segments
-            segments.append(u_seg_vals[:])
-            print ("segmented at ", average[k]) #changes i to k
+            del segment_vals[k+1:]
+            segments.append(segment_vals[:])
+            seg_start.append(segment_vals[0])
+            centre_vals = average[k:i]  #prepend to next segment
+            
+            print ("segmented at ", average[k])
             
             if max(segment_vals) > 1000.0:
                 print("non-silence")
@@ -83,8 +74,7 @@ def segment():
                 print("silence")
             
             del segment_vals[:]
-            del u_seg_vals[:]
-                
+            
             for j in range(i, len(average)):
                 if average[j] < 25000.0:
                     k = j
@@ -98,16 +88,20 @@ def segment():
                             k+=1
                     
                     #while loop breaks where the transition is found, create a new list with both segments' values
-                    u_seg_vals = segment_vals[:] + average[j+1:k+1] #include next from j, include k
+                    u_seg_vals = centre_vals[:] + segment_vals[:] + average[j:k+1]
                     
                     segments.append(u_seg_vals[:])
+                    seg_start.append(u_seg_vals[0])
+                    
                     print ("segmented at ", average[k])
                     print ("non-silence") #this condition can only be met if iterator has passed a value >25000, when going back,
                     #it's obvious the previous region was non-silence
                     
                     del segment_vals[:]
                     del u_seg_vals[:]
-                    i = j
+                    del centre_vals[:]
+                    
+                    i = k
                     break
                 
                 segment_vals.append(average[j])
@@ -115,7 +109,7 @@ def segment():
         else: segment_vals.append(average[i])
         i+=1
         
-    return segments
+    return segments, seg_start
 
 def createPattern():
     pattern = ""
@@ -126,6 +120,21 @@ def createPattern():
         elif max(seg) >=300000: pattern +="v"
         
     return pattern
+
+def plotStuff(data, title, xlabel, ylabel, xlim1= 0, xlim2= 0, ylim1 = 0, ylim2= 0):
+    plt.plot(data)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    if xlim1 != 0 and xlim2 != 0:
+        plt.xlim(xlim1,xlim2)
+        
+    if ylim1 != 0 and ylim2 != 0:
+        plt.ylim(ylim1, ylim2)
+        
+    plt.show()
+
 
 #read audio files
 rate_names, data_names = read("names.wav")
@@ -149,11 +158,21 @@ plotTimAmp(audio, rate_bit)
 # plot spectogram
 freq, time, sx = plotSpec(audio, rate_bit)
 
-#plot high density energy frames, take 5 element window
-average = plotEnergyContour(time, sx)
+#compute high density energy frames, take 5 element window
+average = energyFrames(time, sx)
+plotStuff(average, "Energy contour", "index", "energy")
 
 #recognize and get silence and non-silence regions
-segments = segment()
+segments, seg_start = segment()
 
 #get the pattern string
 pattern = createPattern()
+
+#plot segments over contour
+x = npy.array(average)
+seg_indices = [average.index(i) for i in seg_start]
+y = npy.array(seg_indices)
+
+plt.plot(x)
+plt.plot(y,x[y],'x')
+plt.show()
