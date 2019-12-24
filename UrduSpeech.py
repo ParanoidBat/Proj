a9904e9f9ecc6cddebd426562d53ef1cdcf01374
ParanoidBat <41900396+ParanoidBat@users.noreply.github.com>
2019-12-24 18:06:44 +0500

    zcr integrated, compared with energy contour

--- UrduSpeech.py
+++ UrduSpeech.py
@@ -9,36 +9,36 @@ import VowelRecognizer as vr
 
 pattern = []
 
-#pattern.append(vr.recognizeVowels("kahan ho2.wav"))
-#pattern.append(vr.recognizeVowels("bas aa raha hn2.wav"))
-#pattern.append(vr.recognizeVowels("kya hal hai2.wav"))
-#pattern.append(vr.recognizeVowels("mae theek hn2.wav"))
-#pattern.append(vr.recognizeVowels("message kholo2.wav"))
-#pattern.append(vr.recognizeVowels("phone karo2.wav"))
-#pattern.append(vr.recognizeVowels("salam2.wav"))
-#pattern.append(vr.recognizeVowels("wasalam2.wav"))
-#pattern.append(vr.recognizeVowels("whatsapp chalao2.wav"))
+pattern.append(vr.recognizeVowels("Samples/kahan ho.wav", 1.8))
+pattern.append(vr.recognizeVowels("Samples/bas aa raha hn.wav", 1.8))
+pattern.append(vr.recognizeVowels("Samples/kya hal hai.wav", 1.8))
+pattern.append(vr.recognizeVowels("Samples/mae theek hn.wav", 1.9))
+pattern.append(vr.recognizeVowels("Samples/message kholo.wav", 1.8))
+pattern.append(vr.recognizeVowels("Samples/phone karo.wav", 1.9))
+pattern.append(vr.recognizeVowels("Samples/salam.wav", 1.9))
+pattern.append(vr.recognizeVowels("Samples/wasalam.wav", 1.8))
+pattern.append(vr.recognizeVowels("Samples/whatsapp chalao.wav", 1.9))
 
-pattern.append(vr.recognizeVowels("kahan ho3.wav"))
-pattern.append(vr.recognizeVowels("bas aa raha hn3.wav"))
-pattern.append(vr.recognizeVowels("kya hal hai3.wav"))
-pattern.append(vr.recognizeVowels("mae theek hn3.wav"))
-pattern.append(vr.recognizeVowels("message kholo3.wav"))
-pattern.append(vr.recognizeVowels("phone karo3.wav"))
-pattern.append(vr.recognizeVowels("salam3.wav"))
-pattern.append(vr.recognizeVowels("wasalam3.wav"))
-pattern.append(vr.recognizeVowels("whatsapp chalao3.wav"))
+#pattern.append(vr.recognizeVowels("Samples/kahan ho2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/bas aa raha hn2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/kya hal hai2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/mae theek hn2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/message kholo2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/phone karo2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/salam2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/wasalam2.wav"))
+#pattern.append(vr.recognizeVowels("Samples/whatsapp chalao2.wav"))
 
-patterns = dict()
-patterns["kahan ho"] = pattern[0]
-patterns["bas aa raha hn"] = pattern[1]
-patterns["kya hal hai"] = pattern[2]
-patterns["mae theek hn"] = pattern[3]
-patterns["message kholo"] = pattern[4]
-patterns["phone karo"] = pattern[5]
-patterns["salam"] = pattern[6]
-patterns["wasalam"] = pattern[7]
-patterns["whatsapp chalao"] = pattern[8]
-
-for x,y in patterns.items():
-    print(x,y)
+#patterns = dict()
+#patterns["kahan ho"] = pattern[0]
+#patterns["bas aa raha hn"] = pattern[1]
+#patterns["kya hal hai"] = pattern[2]
+#patterns["mae theek hn"] = pattern[3]
+#patterns["message kholo"] = pattern[4]
+#patterns["phone karo"] = pattern[5]
+#patterns["salam"] = pattern[6]
+#patterns["wasalam"] = pattern[7]
+#patterns["whatsapp chalao"] = pattern[8]
+#
+#for x,y in patterns.items():
+#    print(x,y)
--- VowelRecognizer.py
+++ VowelRecognizer.py
@@ -12,7 +12,7 @@ from scipy import signal
 
 def plotTimAmp(audio, rate):
      plt.plot(npy.arange(audio.shape[0])/rate,audio) # time-amplitude plot
-     plt.title("Audio wave")
+     plt.title(audio)
      plt.xlabel("Time - s")
      plt.ylabel("Amplitude")
      plt.show()
@@ -22,7 +22,7 @@ def plotSpec(audio, rate):
     plt.pcolormesh(time, freq/1000, 10*npy.log10(sx), cmap="viridis") #log the frequency axis for better readability
     plt.xlabel("Time (s)")
     plt.ylabel("Frequency (kHz)")
-    plt.title("Spectrogram - audio")
+    plt.title("Spectrogram")
     #plt.ylim(0,10)
     plt.show()
     
@@ -74,7 +74,6 @@ def segment(energy_frames):
             
             centre_vals = energy_frames[k:i]  # prepend to next segment
             
-            print ("segmented at ", energy_frames[k])
             del segment_vals[:]
             
         else:
@@ -91,7 +90,7 @@ def segment(energy_frames):
                 try:
                     k+=1
                     
-                    if energy_frames[k] <= peak/1.5: # if next value is less than 2/3rd of value in 'peak', we've found the peak
+                    if energy_frames[k] <= peak/1.4: # if next value is less than 2/3rd of value in 'peak', we've found the peak
                         peaks.append(energy_frames.index(peak))
                         break
                     
@@ -105,7 +104,7 @@ def segment(energy_frames):
             # extend the segment to find a transition point
             while k < len(energy_frames):
                 try:
-                    if energy_frames[k] <= peak/1.5: # if the value is atleast 2/3rd of the peak
+                    if energy_frames[k] <= peak/1.4: # if the value is atleast 2/3rd of the peak
                         
                         # find transition point
                         while k < len(energy_frames):
@@ -137,32 +136,42 @@ def segment(energy_frames):
 
 def createPattern(segments, peaks, energy_frames):
     pattern = []
-    i= 0
+    i= j = 0
+    
+    # dont pass empty list
+    if not segments[j]:
+        j+=1
+    
+    # if between range, is a consonant, else a vowel
+    if max(segments[j]) > 1000 and max(segments[j]) <=10000:
+        pattern.append("c")
+        j+=1
+        
+    elif max(segments[j]) > 10000:
+        pattern.append("v")
+        j+=1
+        i=+1 # next block of code recognizes vowels only, we've recognized one here, so update the iterator
     
     for seg in segments:
+        
+        seg = segments[j]
+        
         if seg:
             try:
                 if max(seg) == energy_frames[peaks[i]]: # if there's a peak in the segment, it's a vowel
                     pattern.append("v")
                     i+=1
+                    j+=1
                 
                 else: # we are only looking for vowels, so if not found. continue
+                    j+=1
                     continue
-                
-                if len(pattern) >= 1: # if segment ends at value 2/3rd of peak, means there's another peak close ahead, thus a vowel. Represent 2 vowels with 'V'
-                    if seg[-1:] >= max(seg)/1.7:
-                        pattern[-1:] = "V"
-                        i+=1 # next peak has been taken care of, move onwards
-#                        continue
                     
                 if len(pattern) > 1:
                     # 2 vowels can't be written together (except for above case). if such a case is found, insert a 'c' b/w them
-                    
-                    if (pattern[-2: -1] == "v" or "V" ) and (pattern[-1:] == "v" or "V"): 
-                        if not seg[0] < 1000.0: # if segment terminals haven't dived into silence region
-                            pattern.insert(-1, "c")
-                        else:
-                            pattern.insert(-1, "s")
+
+                    if pattern[-2] == "v" and pattern[-1] == "v":
+                        pattern.insert(-1, "c")
             
             except IndexError:
                 break
@@ -189,18 +198,83 @@ def scaleDown(scale_to, segment, peak): #scale down the energies to give consist
     for i in range(len(segment)):
         segment[i] = segment[i]*factor
         
-def smooth(data, size): #smooth data to eliminate noise
+def smooth(data, size, mean): #smooth data to eliminate noise
     new_data = []
-    i = 3
+    factor = mean//2
+    i = factor
 
-    while(i< size-3):
-        new_data.append(sum(data[i-3:i+3])/7) #take mean of 7 values, taking i'th value as the pivot
+    while(i< size-factor):
+        new_data.append(sum(data[i-factor:i+factor])/mean) #take mean of 'mean' values, taking i'th value as the pivot
         i+=1
         
     return new_data
+
+def zeroCrossingRate(wave):
+    rate = []
     
-def recognizeVowels(audio_sample):
-#audio_sample = "whatsapp chalao2.wav"
+    # taking 19ms window. 924 samples
+    samples = 924
+    
+    above = True # boolean to check if value is above or below zero
+    prev_state = above # remember the previous state to determine zero crossing
+    
+    try:
+        while 1:
+            # don't let go out of bounds
+            if samples >= len(wave) or samples > len(wave) - 924:
+                break
+            
+            counter = 0 # count number of times zero is crossed
+            
+            # set bool to determine crossing
+            if wave[samples - 924] > 0:
+                above = True
+            else:
+                above = False
+                
+            prev_state = above
+            
+            for i in range(samples - 924, samples):
+                if wave[i] > 0:
+                    above = True
+                else:
+                    above = False
+                    
+                if prev_state is not above: # the state changed, meaning zero is crossed
+                    counter+=1
+                    
+                prev_state = above
+                
+            samples +=924
+            rate.append(counter/0.019)
+                
+    except IndexError:
+        rate.append(counter/0.019)
+        print("Error: Index Is Out Of Bounds!")
+        
+    return npy.array(rate)
+
+def mapEnergyToZcr(factor, peaks, segments):
+    indices = []
+    
+    try:
+        for i in range(len(peaks)):
+            indices.append(peaks[i]//factor)
+    except IndexError:
+        pass
+    
+    try:
+        for i in range(len(segments)):
+            indices.append(segments[i]//factor)
+    except IndexError:
+        pass
+    
+    return npy.array(indices, dtype = npy.int32)
+    
+def recognizeVowels(audio_sample, map_factor):
+#    audio_sample = "Samples/kahan ho.wav"
+#    map_factor = 1.8
+            
     sample_rate, wave_data = read(audio_sample)
     data_array = npy.array(wave_data)
     audio = npy.mean(data_array,1) #make it into mono channel
@@ -214,12 +288,17 @@ def recognizeVowels(audio_sample):
     if peak >scale_to:
         scaleDown(scale_to, ef, peak)
     
-    s_ef = smooth(ef, len(ef))
+    s_ef = smooth(ef, len(ef), 7)
     
     segments, seg_start, peaks = segment(s_ef) #get segments, their indices and peaks' indices
     
     pattern = createPattern(segments, peaks, s_ef)
     
+    zero_crossing_rate = zeroCrossingRate(audio)
+    smooth_zcr = npy.array(smooth(zero_crossing_rate, zero_crossing_rate.size, 5))
+    mapped_values = mapEnergyToZcr(map_factor, peaks, seg_start)
+    
+    ##plotting##
     contour = npy.array(s_ef)
     indices = npy.array(seg_start)
     peakses = npy.array(peaks)
@@ -229,10 +308,30 @@ def recognizeVowels(audio_sample):
     plt.plot(indices, contour[indices],'x')
     plt.plot(peakses, contour[peakses], 'v')
     plt.title("Energy contour - "+ audio_sample)
-    plt.xlabel("Index")
+    plt.xlabel("Time - ds (deca sec)")
     plt.ylabel("Energy")
     #plt.ylim(0,1200)
     #plt.xlim(2000,2500)
     plt.show()
     
-    return pattern 
+    # wave
+    plt.figure(figsize=(6, 9)) # x, y size in inches
+    plt.subplots_adjust(hspace = 0.3) # vertical space between subplots in inches
+    
+    plt.subplot(211) # rows, columns, index
+    plt.title("Wave - "+ audio_sample)
+    plt.xlabel("Time - s")
+    plt.ylabel("Amplitude")
+    plt.plot(npy.arange(audio.shape[0])/sample_rate,audio)
+    
+    # zcr
+    plt.subplot(212)
+    plt.plot(zero_crossing_rate)
+    plt.plot(mapped_values, zero_crossing_rate[mapped_values], 'x')
+    plt.title("Zero Crossing Rate - "+ audio_sample)
+    plt.xlabel("Time - hs (hecto sec)")
+    plt.ylabel("Rate")
+    
+    plt.show()
+        
+    return pattern
