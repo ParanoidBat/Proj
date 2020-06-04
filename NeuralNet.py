0a2b49111eb8a2771343bd97987b69798c11ffe0
ParanoidBat <41900396+ParanoidBat@users.noreply.github.com>
2020-06-04 21:46:57 +0500

    vectors populated with data

--- NeuralNet.py
+++ NeuralNet.py
@@ -7,9 +7,14 @@ Created on Thu May 21 16:39:23 2020
 import math
 import numpy as npy
 import random
-import decimal
 
-decimal.getcontext().prec = 100 # to handle very large numbers
+def get_index(r): # used to populate input vector
+    for i in range(r):
+        yield i
+
+indices = {"s": 0, "v": 1, "c": 2, "f": 3} # used to set ouput vector
+
+# nueral net functions
 
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
@@ -31,9 +36,9 @@ def shuffle(arr, n):
     return None
 
 ############################
-NUM_INPUTS = 2
-NUM_HIDDEN_NODES = 2
-NUM_OUTPUTS = 1
+NUM_INPUTS = 278 # each sample has 278 elements
+NUM_HIDDEN_NODES = 278
+NUM_OUTPUTS = 4 # s, v, c, f
 LR = 0.1
 
 hidden_layer = [0]*NUM_HIDDEN_NODES
@@ -42,13 +47,42 @@ output_layer = [0]*NUM_OUTPUTS
 hidden_layer_bias = [0]*NUM_HIDDEN_NODES
 output_layer_bias = [0]*NUM_OUTPUTS
 
-hidden_weights = npy.zeros((2,2)) # 2d matrix
-output_weights = npy.zeros((2,1))
+hidden_weights = npy.zeros((278,278)) # 2d matrix
+output_weights = npy.zeros((278,4))
 
-NUM_TRAINING_SETS = 4
+NUM_TRAINING_SETS = 1737
 
-training_inputs = npy.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0]], dtype = npy.float64)
-training_outputs = npy.array([[0.0], [1.0], [1.0], [0.0]], dtype = npy.float64)
+#training_inputs = npy.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0]], dtype = npy.float64)
+#training_outputs = npy.array([[0.0], [1.0], [1.0], [0.0]], dtype = npy.float64)
+
+training_inputs = npy.zeros((NUM_TRAINING_SETS, NUM_INPUTS))
+training_outputs = npy.zeros((NUM_TRAINING_SETS, NUM_OUTPUTS))
+
+files = ["ef_peaks.txt", "ef_segs.txt", "zcr_peaks.txt", "zcr_segs.txt"]
+
+# populate input/output vectors
+i = get_index(NUM_TRAINING_SETS)
+
+for f in files:
+    with open (f, "r") as file:
+        
+        for line in file:
+            tmp = line.rstrip("\n").split(",")
+            tmp2 = ""
+            for e in tmp[-1:]: tmp2+= e
+            
+            del tmp[len(tmp) - 1 :]
+            tmp = list(map(float, tmp))
+            
+            # set the input vector
+            try: index = next(i)
+            except: pass
+            
+            for j in range(278):
+                training_inputs[index, j] = tmp[j]
+            
+            # set output vector
+            training_outputs[index, indices.get(tmp2)] = 1
 
 # initialize hidden weigths
 for i in range(NUM_INPUTS):
@@ -66,7 +100,7 @@ for i in range(NUM_HIDDEN_NODES):
 for i in range(NUM_OUTPUTS):
     output_layer_bias[i] = init_weight()
 
-training_set_order = [0, 1, 2, 3]
+training_set_order = [x for x in range(NUM_TRAINING_SETS)]
 
 # training
 for n in range(10000):
@@ -92,7 +126,7 @@ for n in range(10000):
             
             output_layer[j] = sigmoid(activation)
         
-        print("input:", training_inputs[i, 0], training_inputs[i,1], "output:", output_layer[0], "expected output:", training_outputs[i,0])
+        print("input:", training_inputs[i, :], "output:", output_layer[0], "expected output:", training_outputs[i, :])
         
         
         # back propogation
